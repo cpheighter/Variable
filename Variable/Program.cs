facdddce92e48a56eec2043fd020a6e3142a5949
cpheighter <cheighterjr@gmail.com>
2022-07-01 12:27:00 -0400

    git init-1st commit

---
+++ Variable/Program.cs
@@ -0,0 +1,8 @@
+﻿string name = "Chris";
+int age = 24;
+char letter = 'C';
+bool isTrue = true;
+double number = 0;
+decimal pi = 3.14m;
+
+    Console.WriteLine($"My name is {name} \n{name} is {age} years old \n{name} name starts with a {letter} \n{name} favorite number is {number} \n{name} know that pi is {pi} \nAll of these statements are {isTrue}");
